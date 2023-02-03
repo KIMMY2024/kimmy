6496b8c388c6fc64ab1c91b64e2789139258e7a5
adelmar526 <adelmar526@gmail.com>
2023-02-03 21:16:35 +0000

    vue.js

--- App/vue.js
+++ App/vue.js
@@ -3,12 +3,32 @@ const API_URL = 'https://web-app-cw2.herokuapp.com/api'
 let shop = new Vue({
     el: "#app",
     data: {
-        lesson: [],
+        lessons: [],
         sortBy: "topic",
         sortopt: "asc",
         currentp: "lessons",
         name: "",
         phone: "",
+        selectLesson: null,
+        searchL: "",
+
+    },
+    methods: {
+        togglePage() { // allows us to swap between the pages
+            if (this.currentp === "lessons") {
+                this.currentp = "confirm";
+            } else {
+                this.currentp = "lessons"
+            }
+        },
+        buyLesson(lesson) { // this allows the lesson confirmation to come up before booking the lesson
+            this.selectLesson = lesson;
+            this.togglePage();
+        },
+        cancelLesson() {
+            this.selectLesson = null;
+            this.togglePage();
+        },
 
     }
 
