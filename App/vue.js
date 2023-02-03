9893a1a5f58b2ad94434f288d1d5ca2fa1f74767
adelmar526 <adelmar526@gmail.com>
2023-02-03 14:24:50 +0000

    vue.js

--- App/vue.js
+++ App/vue.js
@@ -1,1 +1,13 @@
 const API_URL = 'https://web-app-cw2.herokuapp.com/api'
+
+let shop = new Vue({
+    el: "#app",
+    data: {
+        lesson: [],
+        sortBy: "topic",
+        sortopt: "asc",
+        currentp: "lessons",
+        name
+    }
+
+});
